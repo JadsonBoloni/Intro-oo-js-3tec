bdb88cd4c797a9c65c5c879d121c20794745614f
JadsonBoloni <jadson.boloni.santos@escola.pr.gov.br>
2026-09-11 13:20:05 +0000

    objetos

---
+++ objetosLiteral.js
@@ -0,0 +1,28 @@
+const user = {
+  nome: "Jadson",
+  email: "jadson@jadson.com",
+  nascimento: "2009/02/02",
+  role: "admin",
+  ativo: true,
+  exibirInfos: function() {
+    console.log(this.nome, this.email)
+  }
+}
+
+const admin = {
+  nome: "Mariana",
+  email: "m@m.com",
+  role: "admin",
+  criarCurso() {
+    console.log('curso criado!')
+  }
+}
+
+Object.setPrototypeOf(admin, user)
+admin.criarCurso()
+admin.exibirInfos()
+
+
+
+
+
